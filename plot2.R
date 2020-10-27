a1ea4181368da82ec8625097867af504f74929ed
Roberth Araujo <araujo.ro97@gmail.com>
2020-10-27 17:55:11 -0500

    Complete case 2.

--- plot2.R
+++ plot2.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(lubridate)
 library(dplyr)
 
@@ -20,26 +19,3 @@ dev.off()
 
 # Ver gráfica en png
 file.show("plot2.png")
-=======
-library(lubridate)
-library(dplyr)
-
-# Gráfico 2
-
-# Lectura de datos
-
-setwd("C:/Users/Pavilion/Desktop/Coursera/Curso de ciencia de datos/4. Análisis exploratorio de datos/Proyecto semana 1")
-data<-read.table(file="household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), na.strings = "?")
-data$Date<-dmy(data$Date)
-datacom<-subset(data, data$Date>="2007-02-01" & data$Date<="2007-02-02")%>%
-        mutate(datetime=ymd_hms(paste(Date, Time)))
-
-
-# Hacer tramas 
-png(filename="plot2.png", width = 480, height = 480)
-plot(datacom$Global_active_power~datacom$datetime,col = "black", ylab = "Global Active Power (kilowatts)", xlab = "", type="l")
-dev.off()
-
-# Ver gráfica en png
-file.show("plot2.png")
->>>>>>> 839e800c0e871d9027cba0fda7841e3f660ba247
